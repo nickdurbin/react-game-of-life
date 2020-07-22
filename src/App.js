f9cf0f78b29b3ca3b883a6ed3f969980f0c0bbed
durbcodes <nick.durbin@gmail.com>
2020-07-22 10:15:58 -0400

    Brought in what will be the home view for the app.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Home from './views/Home'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Home />
         <h1>Game of Life</h1>
       </header>
     </div>
