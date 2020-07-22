2ac9bad699a364248636035b4cf91c4961a9d0e3
durbcodes <nick.durbin@gmail.com>
2020-07-22 10:40:55 -0400

    Just messing with some settings to see what is possible.

--- src/components/Canvas/Canvas.js
+++ src/components/Canvas/Canvas.js
@@ -10,12 +10,13 @@ function Canvas() {
     <div className='canvas-wrapper'>
       <Sketch          
 					setup={(p5, parentRef) => {
-						p5.createCanvas(200, 200).parent(parentRef);
+						p5.createCanvas(800, 400).parent(parentRef);
 					}}
 					draw={p5 => {
 						p5.background(0);
-						p5.fill(255, y * 1.3, 0);
-						p5.ellipse(p5.width / 2, y, 50);
+            p5.fill(255, y * 1.3, 0);
+            p5.ellipse(p5.width / 2, y, 50);
+            p5.ellipse(p5.width / 4, y, 100);
 						if (y > p5.height) direction = '';
 						if (y < 0) {
 							direction = '^';
